4025c7cf98bbf38b1668aaf971e2641f80927a21
Pawel Rachwalski <eracpaw@localhost.localdomain>
2022-11-21 16:29:23 -0500

    second seed

---
+++ seed2.groovy
@@ -0,0 +1,19 @@
+def names = [ "number1, number2, number3"]
+
+names.each{jobname->
+pipelineJob("jobdsltest_pipeline_$jobname") {
+    description('BLABLA')
+    logRotator {
+      daysToKeep(7)
+    }
+    triggers {
+      cron('@daily')
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace('groovy_scripts/tool_mailman.groovy'))
+            sandbox()
+        }
+    }
+ }
+}
